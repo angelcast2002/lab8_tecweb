63ef3e7edadc2c462c6b11c5961a665171a5065a
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2023-05-03 11:03:49 -0600

    Creacion del documento

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'semi':['error','never'],
+    'max-len':['error',{ 'code':120}],
+    'prefer-arrow-callback': 'error',
+    'quotes': ['error', 'double'],
+    'react/function-component-definition': 'off'
+  },
+};
