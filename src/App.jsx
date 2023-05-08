afb3d9e80a99cde9c2891749223934ae96bf8802
angelcast2002 <castellanosa80@gmail.com>
2023-05-07 19:26:39 -0600

    navegacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { StoreContext } from "storeon/react"
+import store from "@store"
 import React from "react"
 import Menu from "./pages/mainMenu/Menu"
 import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <Menu />
-    </div>
+    <StoreContext.Provider value={store}>
+      <div className="App">
+        <Menu />
+      </div>
+    </StoreContext.Provider>
   )
 }
 
